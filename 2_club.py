719404d51823c4c54de34fd172f476cb4713ddbe
mikagekirio <125043119+mikagekirio@users.noreply.github.com>
2024-05-15 14:54:22 +0200

    Update 2_club.py

--- 2_club.py
+++ 2_club.py
@@ -1,55 +1,10 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+
 file=open("/home/jade/ESME/Advanced Algorithms (python)/APP3/club.txt","r")
 lines=file.readlines()
 dic={}
-
-#create a dict a key and the values to which they are inked
-for l in lines[1:]: # to except the tittles
-    if "\n" in l: 
-        l=l[:-1] # to except the 'to the line'
-    line=l.split(",")
-    if int(line[0]) not in dic:
-        dic[int(line[0])]=[int(line[1])]
-    else:
-        dic[int(line[0])].append(int(line[1]))
-        
-def adjacency(dic): # make the adjacent matrix (undirected)
-    adj=np.zeros((34,34),dtype=int)
-    for i in lines[1:]:
-        for j in range (l):
-            if adj[i][j]==1:
-                lead[j]+= 1
-    
-    #for key in dic:
-        #for val in dic[key]:
-            #adj[key-1][val-1]=1
-    return adj
-
-print(adjacency(dic))
-
-'''
-def bfs(G, s):
-    color = dict()
-    for x in G:
-        color[x] = 'white'
-    path = []
-    color[s] = 'grey'
-    history = [s]
-    while history != []:
-        current_vertex = history[0]
-        path.append(current_vertex)
-        for neighbor in G[current_vertex]:
-            if color[neighbor] == 'white':
-                color[neighbor] = 'grey'
-                history.append(neighbor)
-        history.pop(0)
-        color[current_vertex] = 'black'
-    return path
-
-print(bfs(dic, 'B'))
-'''
-
-
 
 #Question 1 et 2 
 
@@ -57,7 +12,7 @@ file = open("/Users/Victoria/Desktop/club.txt", "r")
 lines = file.readlines()
 dic = {}
 
-#create a dict a key and the values to which they are inked (no changes from jade)
+#create a dict a key and the values to which they are linked
 for l in lines[1:]:  
     if "\n" in l:
         l = l[:-1] 
@@ -67,7 +22,7 @@ for l in lines[1:]:
     else:
         dic[int(line[0])].append(int(line[1]))
 
-#create adjacent matrix => changes a little from jade bc it did not work 
+#create adjacent matrix
 def adjacency(dic):
     adj = np.zeros((34, 34), dtype=int)
     for key, values in dic.items():
@@ -88,10 +43,6 @@ top_two_leaders = np.argsort(node_importance)[-2:][::-1] + 1
 print( top_two_leaders)
 
 #Question 5
-
-import numpy as np
-import matplotlib.pyplot as plt
-import networkx as nx
 
 G = nx.Graph()
 
